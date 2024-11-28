bfa501e2caf9336be30b65b9ca86ccef6076a0e8
jorvergara <jvergare@DESKTOP-RSHAG5M.localdomain>
2024-11-28 11:16:59 -0300

    triangulo en cpp y octree contructor y insertar escena en cpp (más rapido)

---
+++ build/CMakeFiles/octree_module.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jvergare/TGVD-Proyecto-Interseccion/octree.cpp" "CMakeFiles/octree_module.dir/octree.cpp.o" "gcc" "CMakeFiles/octree_module.dir/octree.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/octree_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/octree_module.dir/octree.cpp.o"
+  "CMakeFiles/octree_module.dir/octree.cpp.o.d"
+  "octree_module.cpython-39-x86_64-linux-gnu.so"
+  "octree_module.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/octree_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
